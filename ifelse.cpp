37680be33beaf0bd99fd0fa1973a4ed8755c757d
amandeepbera <44435114+amandeepbera@users.noreply.github.com>
2022-07-24 16:46:40 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int savings;
+    cin>>savings;
+
+    if (savings>5000)
+    
+       if (savings>10000)
+       {
+         cout<<"roadtrip with  neha";
+       }
+       else{
+        cout<<"shopping with neha";
+       }
+
+
+    else if(savings>2000)
+    cout<<"rashmi";
+    
+    else
+    cout<<"friends";
+    
+    return 0;
+}
